6bc521bee160226186004dc44167189e333cd106
GwangCheonLee <raitmid56@gmail.com>
2024-04-30 21:34:09 +0900

    feat: Add endpoint query parameter example

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/items")
+async def read_item(skip: int = 0, limit: int = 10):
+    return {"skip": skip, "limit": limit}
+
+
 @app.get("/items/{item}")
 def get_item(item: int):
     return {"item": item}
