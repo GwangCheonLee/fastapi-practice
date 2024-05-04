d0dfc992c80c8839fb9c3350423d8227bcbe0c28
GwangCheonLee <raitmid56@gmail.com>
2024-05-04 12:38:32 +0900

    chore: moved file users_controller.py to users folder

--- users/users_controller.py
+++ users/users_controller.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from config.database import Session, get_db
 
 router = APIRouter()
 
 
 @router.get("/users/", tags=["users"])
-async def read_users():
+async def read_users(session: Session = Depends(get_db)):
+    session
     return [{"username": "Rick"}, {"username": "Morty"}]
 
 
