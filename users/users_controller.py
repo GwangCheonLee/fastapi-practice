6affea8293761d7f3906a0e279d385c5196a5372
GwangCheonLee <raitmid56@gmail.com>
2024-05-04 20:13:40 +0900

    feat: remove mok method and connect users_service

--- users/users_controller.py
+++ users/users_controller.py
@@ -1,21 +1,16 @@
 from fastapi import APIRouter, Depends
 
 from config.database import Session, get_db
+from users import users_service
 
 router = APIRouter()
 
 
-@router.get("/users/", tags=["users"])
+@router.get("/users", tags=["users"])
 async def read_users(session: Session = Depends(get_db)):
-    session
-    return [{"username": "Rick"}, {"username": "Morty"}]
+    return users_service.get_users(session)
 
 
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
-
-
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+@router.get("/users/{user_id}", tags=["users"])
+async def get_one_user_by_id(user_id: int, session: Session = Depends(get_db)):
+    return users_service.get_one_user_by_id(session, user_id)
