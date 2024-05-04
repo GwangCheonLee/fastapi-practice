ab195d3dfa4a1d1b6e958a0456943124bad1095c
GwangCheonLee <raitmid56@gmail.com>
2024-05-04 20:12:59 +0900

    refactor: Method rename and add return type

--- users/repositories/user_repository.py
+++ users/repositories/user_repository.py
@@ -6,12 +6,8 @@ class UserRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def add_user(self, user):
-        self.session.add(user)
-        self.session.commit()
-
-    def get_user_by_id(self, user_id):
-        return self.session.query(User).filter(User.id == user_id).one()
-
-    def get_all_users(self):
+    def get_users(self) -> list[User]:
         return self.session.query(User).all()
+
+    def get_one_user_by_id(self, user_id: int) -> User:
+        return self.session.query(User).filter(User.id == user_id).one()
