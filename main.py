3b011292513e5e420fe2d8dcdafd95614c1dde07
GwangCheonLee <raitmid56@gmail.com>
2024-05-04 12:40:41 +0900

    refactor: User router import statement

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from routers import users
+from users import users_controller
 
 app = FastAPI()
-app.include_router(users.router)
+app.include_router(users_controller.router)
 
 
 @app.get("/")
