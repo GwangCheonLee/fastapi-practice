7e564b1bcc1ea441054007f80c082ed819d8ead4
GwangCheonLee <raitmid56@gmail.com>
2024-05-05 12:38:35 +0900

    feat: Include auth_controller

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from auth import auth_controller
 from users import users_controller
 
 app = FastAPI()
 app.include_router(users_controller.router)
+app.include_router(auth_controller.router)
 
 
 @app.get("/")
