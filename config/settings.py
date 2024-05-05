da2eb32c3e49c5345cf8abf6ba9ced12689a72a4
GwangCheonLee <raitmid56@gmail.com>
2024-05-05 12:25:24 +0900

    feat: Add dependency python-jose

--- config/settings.py
+++ config/settings.py
@@ -3,4 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    jwt_secret: str
+    jwt_algorithm: str
+    jwt_expires: int
     model_config = SettingsConfigDict(env_file=".env")
